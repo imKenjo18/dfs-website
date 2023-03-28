989bbc13b524bcf054bd2c0d37455310ec40539d
Kenjo <regenciajoshua123@gmail.com>
2023-03-28 14:56:54 +0800

    Reverted back Bootstrap version
    - Previous commit was upgrading Bootstrap version to 5.0.2 from 4.6.2 for some pages.

--- account-settings.php
+++ account-settings.php
@@ -122,7 +122,7 @@ if ($accCheckResult == 1) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Change Password</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <!-- <link rel="stylesheet" href="assets/style.css"> -->
 </head>
 <body style="
